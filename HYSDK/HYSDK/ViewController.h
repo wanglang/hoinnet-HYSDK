1763b9f93ebb3d08800243ced4cee74493680184
wanglang <wanglang.fj@gmail.com>
2016-01-12 23:45:50 +0800

    create object

---
+++ HYSDK/HYSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYSDK
+//
+//  Created by wolf.wang on 16/1/12.
+//  Copyright © 2016年 hoinnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYSDK/HYSDK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HYSDK
+//
+//  Created by wolf.wang on 16/1/12.
+//  Copyright © 2016年 hoinnet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
